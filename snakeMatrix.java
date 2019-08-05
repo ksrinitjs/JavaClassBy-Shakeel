1837135683266335e49f60153d73c7956652e187
ksrinitjs <53040794+ksrinitjs@users.noreply.github.com>
2019-08-05 12:19:21 +0530

    Add files via upload
    Assignment on Arrays

---
+++ snakeMatrix.java
@@ -0,0 +1,32 @@
+package com.ford.Javaclass;
+
+public class snakeMatrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[][] myArr= {{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
+		int len=myArr.length;
+		//System.out.println(len);
+		for(int i=0;i<len;i++)
+		{
+			if(i%2==0)
+			{
+			for(int j=0;j<len;j++)
+			{
+				System.out.print(myArr[i][j]+"-> ");
+			}
+			}
+			else
+			{
+				for(int j=len-1;j>=0;j--)
+				{
+					System.out.print(myArr[i][j]+"->");
+				}	
+			}
+		}
+		
+
+	}
+
+}
